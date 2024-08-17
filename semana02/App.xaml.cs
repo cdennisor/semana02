182d8621516a6360836f7a747a52389c3df1ca3a
cdennisor <1479649@senati.pe>
2024-08-17 09:27:59 -0500

    Initial commit

---
+++ semana02/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using semana02.View.Home;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana02
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new VHPrincipal();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
